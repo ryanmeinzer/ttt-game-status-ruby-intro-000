fff4fa5b9238a9b129de936448745f3e7ed85daa
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-03-17 15:42:16 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,26 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2], # Top Row
+  [3,4,5], # Middle Row
+  [6,7,8], # Bottom Row
+  [0,3,6], # Left Column
+  [1,4,7], # Middle Column
+  [2,5,8], # Right Column
+  [0,4,8], # Top-Left-Bottom-Right Diagnoal
+  [2,4,6] # Top-Right-Bottom-Left Diagnoal
+]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+      position_taken?(board, combo[0]) && board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]]
+  end
+end
+
+def full?(board)
+  board.none? do |player|
+    player != "X" && player != "O"
+  end
+end
